980e677ba316edeaffb3568d2696c2f35cfdeca9
bang9 <gusrn1423@naver.com>
2023-04-15 19:00:07 +0900

    chore: added test environment

--- src/__tests__/run.test.ts
+++ src/__tests__/run.test.ts
@@ -1,4 +1,14 @@
 import { LottoService } from '../app';
 
-const richAutomation = new LottoService();
-richAutomation.signIn('YOUR_ID', 'YOUR_PASSWORD');
+const sec = (n: number) => 1000 * n;
+
+describe('run', function () {
+  it(
+    'should be able to sign in',
+    async () => {
+      const richAutomation = new LottoService();
+      await richAutomation.signIn('YOUR_ID', 'YOUR_PASSWORD');
+    },
+    sec(10)
+  );
+});
