2fc19e9ad6f6ebd8c468c1511b48a2c7fca8bc60
Hyungu Kang | Airen <gusrn1423@naver.com>
2023-04-15 19:32:02 +0900

    chore: added test environment (#3)
    * chore: added test environment
    
    * chore: added environment vars
    
    * skip

--- src/__tests__/run.test.ts
+++ src/__tests__/run.test.ts
@@ -1,4 +1,21 @@
+import * as dotenv from 'dotenv';
 import { LottoService } from '../app';
+dotenv.config();
 
-const richAutomation = new LottoService();
-richAutomation.signIn('YOUR_ID', 'YOUR_PASSWORD');
+const sec = (n: number) => 1000 * n;
+const { LOTTO_ID, LOTTO_PWD } = process.env;
+describe('run', function () {
+  it('should have env variables', () => {
+    expect(LOTTO_ID).toBeDefined();
+    expect(LOTTO_PWD).toBeDefined();
+  });
+
+  it(
+    'should be able to sign in',
+    async () => {
+      const richAutomation = new LottoService();
+      await richAutomation.signIn(LOTTO_ID, LOTTO_PWD);
+    },
+    sec(10)
+  );
+});
