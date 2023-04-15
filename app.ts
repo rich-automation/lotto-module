cf8b56e59e4ce93211d33f935299be94d39c7eb9
DongGukMon <30457954+DongGukMon@users.noreply.github.com>
2023-04-15 17:55:13 +0900

    feat: ts setting & lotto service (#1)
    * Feat: ts setting & lotto service
    * Feat: BrowserController 구현
    * Feat: constants/selectors로 상수 분리
    * Feat: browserController 외부에서 page 객체를 주입받지 않아도 default page를 사용할 수 있도록 변경

---
+++ app.ts
@@ -0,0 +1,5 @@
+import { LottoService } from "./lottoService";
+
+const richAutomation = new LottoService();
+
+richAutomation.signIn("YOUR_ID", "YOUR_PASSWORD");
---
+++ lottoService.ts
@@ -0,0 +1,66 @@
+import { BrowserController } from "./browserController";
+import { Dialog } from "puppeteer";
+import { SELECTORS } from "./constants/selectors";
+
+interface LottoServiceInterface {
+  signIn(id: string, password: string): void;
+  purchase(count: number): Promise<number[][]>;
+  check(numbers: number[]): Promise<void>;
+}
+
+export class LottoService implements LottoServiceInterface {
+  BrowserController = new BrowserController({
+    headless: false,
+    args: ["--window-size-1920,1080"],
+    slowMo: 30,
+  });
+
+  async signIn(id: string, password: string): Promise<void> {
+    const page = await this.BrowserController.getNewPage();
+
+    await this.BrowserController.setViewPortSize({
+      width: 1080,
+      height: 1024,
+    });
+
+    await Promise.all([
+      this.BrowserController.navigateWithUrl(SELECTORS.LOGIN_URL),
+      this.BrowserController.waitForNavigation(),
+    ]);
+
+    await this.BrowserController.fillInput(SELECTORS.ID_INPUT_SELECTOR, id);
+    await this.BrowserController.fillInput(
+      SELECTORS.PWD_INPUT_SELECTOR,
+      password
+    );
+    await this.BrowserController.clickForm(SELECTORS.LOGIN_BUTTON_SELECTOR);
+
+    const browser = await this.BrowserController.getBrowser();
+
+    const onShowDialog = (dialog: Dialog) => {
+      //아이디 또는 비밀번호를 잘못 입력했을 경우 브라우저를 닫고 종료한다.
+      if (dialog.message() === "아이디 또는 비밀번호를 잘못 입력하셨습니다") {
+        console.error("아이디 또는 비밀번호를 잘못 입력하셨습니다");
+        return browser.close();
+      }
+    };
+
+    await this.BrowserController.onShowDialog(onShowDialog);
+
+    await Promise.all([
+      this.BrowserController.waitForNavigation(),
+      this.BrowserController.waitForTime(1000),
+    ]);
+
+    // 팝업 제거용
+    await this.BrowserController.cleanPages([1]);
+  }
+
+  async purchase(count: number): Promise<number[][]> {
+    return await [[1, 2, 3, 4, 5, 6]];
+  }
+
+  async check(numbers: number[]): Promise<void> {
+    return await console.log("");
+  }
+}
