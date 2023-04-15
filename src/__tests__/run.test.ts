454e2314061af6905848d2ecd8684077eaf393f9
bang9 <gusrn1423@naver.com>
2023-04-15 19:06:47 +0900

    chore: added environment vars

--- src/__tests__/run.test.ts
+++ src/__tests__/run.test.ts
@@ -1,13 +1,21 @@
+import { config } from 'dotenv';
 import { LottoService } from '../app';
 
+const { LOTTO_ID, LOTTO_PWD } = process.env;
 const sec = (n: number) => 1000 * n;
+config();
 
 describe('run', function () {
+  it('should have env variables', () => {
+    expect(LOTTO_ID).not.toBeDefined();
+    expect(LOTTO_PWD).not.toBeDefined();
+  });
+
   it(
     'should be able to sign in',
     async () => {
       const richAutomation = new LottoService();
-      await richAutomation.signIn('YOUR_ID', 'YOUR_PASSWORD');
+      await richAutomation.signIn(LOTTO_ID, LOTTO_PWD);
     },
     sec(10)
   );
