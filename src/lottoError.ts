121b4079c3b9d9df6aa9b41c45449b34c9a70361
bang9 <gusrn1423@naver.com>
2023-04-17 02:32:19 +0900

    test: added lottoError test

--- src/lottoError.ts
+++ src/lottoError.ts
@@ -7,8 +7,7 @@ const BaseErrorCode = {
 
 const LoginErrorCode = {
   CREDENTIALS_INCORRECT: 200001,
-  INVALID_COOKIE: 200002,
-  TOO_MANY_WRONG_SIGN_IN: 200003
+  INVALID_COOKIE: 200002
 } as const;
 
 const ErrorCode = { ...BaseErrorCode, ...LoginErrorCode };
@@ -20,8 +19,7 @@ const ErrorMessage: Record<ErrorCodeNumber, string> = {
   [ErrorCode.NETWORK_ERROR]: '네트워크 에러가 발생했습니다.',
   [ErrorCode.UNKNOWN_ERROR]: '알 수 없는 오류가 발생했습니다.',
   [ErrorCode.CREDENTIALS_INCORRECT]: '아이디 혹은 비밀번호가 일치하지 않습니다.',
-  [ErrorCode.INVALID_COOKIE]: '쿠키가 만료됐거나 유효하지 않습니다.',
-  [ErrorCode.TOO_MANY_WRONG_SIGN_IN]: '로그인에 너무 많이 실패했습니다. 비밀번호 찾기를 이용하세요.'
+  [ErrorCode.INVALID_COOKIE]: '쿠키가 만료됐거나 유효하지 않습니다.'
 };
 
 export default class LottoError extends Error {
@@ -37,16 +35,20 @@ export default class LottoError extends Error {
   static InvalidCookies() {
     return new LottoError(ErrorCode.INVALID_COOKIE);
   }
-  static TooManyWrongSignIn() {
-    return new LottoError(ErrorCode.TOO_MANY_WRONG_SIGN_IN);
+
+  static get code() {
+    return ErrorCode;
   }
-  static getErrorMessage(code: ErrorCodeNumber) {
+  static getMessage(code: ErrorCodeNumber) {
     return ErrorMessage[code];
+  }
+  static getName(code: ErrorCodeNumber) {
+    return ErrorName[code];
   }
 
   public code: number;
   constructor(code: ErrorCodeNumber, message?: string) {
-    super(ErrorMessage[code] ?? message);
+    super(message ?? ErrorMessage[code]);
     this.name = ErrorName[code] ?? 'LottoError';
     this.code = code;
   }
