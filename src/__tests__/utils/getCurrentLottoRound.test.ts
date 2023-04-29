6652847c9b7664831cd7b58356c0e623aba9579e
bang9 <gusrn1423@naver.com>
2023-04-29 23:00:06 +0900

    test(getCurrentLottoRound): fixed mocking

--- src/__tests__/utils/getCurrentLottoRound.test.ts
+++ src/__tests__/utils/getCurrentLottoRound.test.ts
@@ -2,10 +2,14 @@ import { CONST } from '../../constants';
 import { getCurrentLottoRound } from '../../utils/getCurrentLottoRound';
 
 describe('getCurrentLottoRound', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should return 1000 when the current date is before the first lotto round', () => {
     // Set a fixed date for testing purposes
     const fixedDate = new Date(CONST.THOUSAND_ROUND_DATE);
-    global.Date.now = jest.fn(() => fixedDate) as any;
+    Date.now = jest.fn(() => fixedDate.getTime());
 
     const expectedRound = 1000;
     const result = getCurrentLottoRound();
