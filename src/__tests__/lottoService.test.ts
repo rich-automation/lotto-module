4619380e8c47d1d6f67d1772f63b53a996f988ea
bang9 <gusrn1423@naver.com>
2023-04-29 21:54:17 +0900

    test(lottoService): split tests by method

--- src/__tests__/lottoService.test.ts
+++ src/__tests__/lottoService.test.ts
@@ -17,103 +17,134 @@ describe('lottoService', function () {
     expect(LOTTO_COOKIE).toBeDefined();
   });
 
-  it(
-    'should return valid cookies when sign-in with correct id and password',
-    async () => {
-      const lottoService = new LottoService(configs);
+  describe('signIn', () => {
+    it(
+      'should return valid cookies when sign-in with correct id and password',
+      async () => {
+        const lottoService = new LottoService(configs);
 
-      validCookies = await lottoService.signIn(LOTTO_ID, LOTTO_PWD);
-      expect(validCookies).toBeDefined();
+        validCookies = await lottoService.signIn(LOTTO_ID, LOTTO_PWD);
+        expect(validCookies).toBeDefined();
 
-      await lottoService.destroy();
-    },
-    seconds(60)
-  );
+        await lottoService.destroy();
+      },
+      seconds(60)
+    );
 
-  it(
-    'should throw an exception when sign-in with incorrect id and password',
-    async () => {
-      const lottoService = new LottoService(configs);
-      const incorrectID = Math.random().toString(16).slice(2, 8);
-      const incorrectPWD = '123456';
+    it(
+      'should throw an exception when sign-in with incorrect id and password',
+      async () => {
+        const lottoService = new LottoService(configs);
+        const incorrectID = Math.random().toString(16).slice(2, 8);
+        const incorrectPWD = '123456';
 
-      try {
-        await lottoService.signIn(incorrectID, incorrectPWD);
-      } catch (e) {
-        expect(e).toBeDefined();
-      }
+        try {
+          await lottoService.signIn(incorrectID, incorrectPWD);
+        } catch (e) {
+          expect(e).toBeDefined();
+        }
 
-      await lottoService.destroy();
-    },
-    seconds(60)
-  );
+        await lottoService.destroy();
+      },
+      seconds(60)
+    );
+  });
 
-  it(
-    'should return valid cookies when sign in with valid cookie',
-    async () => {
-      const lottoService = new LottoService(configs);
+  describe('signInWithCookie', () => {
+    it(
+      'should return valid cookies when sign in with valid cookie',
+      async () => {
+        const lottoService = new LottoService(configs);
 
-      const cookies = await lottoService.signInWithCookie(validCookies ?? LOTTO_COOKIE);
-      expect(cookies).toBeDefined();
+        const cookies = await lottoService.signInWithCookie(validCookies ?? LOTTO_COOKIE);
+        expect(cookies).toBeDefined();
 
-      await lottoService.destroy();
-    },
-    seconds(30)
-  );
+        await lottoService.destroy();
+      },
+      seconds(30)
+    );
 
-  it(
-    'should throw an exception when sign in with invalid cookie',
-    async () => {
-      const lottoService = new LottoService(configs);
-      const invalidCookies = '[]';
+    it(
+      'should throw an exception when sign in with invalid cookie',
+      async () => {
+        const lottoService = new LottoService(configs);
+        const invalidCookies = '[]';
 
-      try {
-        await lottoService.signInWithCookie(invalidCookies);
-      } catch (e) {
-        expect(e).toEqual(LottoError.InvalidCookies());
-      }
+        try {
+          await lottoService.signInWithCookie(invalidCookies);
+        } catch (e) {
+          expect(e).toEqual(LottoError.InvalidCookies());
+        }
 
-      await lottoService.destroy();
-    },
-    seconds(30)
-  );
+        await lottoService.destroy();
+      },
+      seconds(30)
+    );
+  });
 
-  it(
-    'run lotto service check',
-    async () => {
-      const lottoService = new LottoService(configs);
+  describe('check', () => {
+    it(
+      'should return valid rank and matched numbers',
+      async () => {
+        const lottoService = new LottoService(configs);
 
-      const cases = [
-        { myNumber: [9, 11, 27, 36, 38, 41], round: 1061, expect: { rank: 0, matchedNumbers: [27] } },
-        { myNumber: [3, 8, 29, 31, 34, 45], round: 1060, expect: { rank: 0, matchedNumbers: [3, 45] } },
-        { myNumber: [9, 14, 24, 25, 33, 34], round: 1059, expect: { rank: 0, matchedNumbers: [25, 34] } },
-        {
-          myNumber: [3, 6, 22, 23, 24, 38],
-          round: 1063,
-          expect: { rank: 1, matchedNumbers: [3, 6, 22, 23, 24, 38] }
-        },
-        {
-          myNumber: [11, 23, 25, 30, 32, 42],
-          round: 1058,
-          expect: { rank: 2, matchedNumbers: [11, 23, 25, 30, 32, 42] }
-        },
-        { myNumber: [2, 22, 26, 29, 30, 34], round: 1053, expect: { rank: 3, matchedNumbers: [22, 26, 29, 30, 34] } },
-        { myNumber: [2, 15, 17, 20, 33, 42], round: 1047, expect: { rank: 4, matchedNumbers: [2, 20, 33, 42] } },
-        { myNumber: [5, 17, 26, 19, 33, 41], round: 1052, expect: { rank: 5, matchedNumbers: [5, 17, 26] } },
-        { myNumber: [1, 5, 17, 26, 19, 33], round: 1052, expect: { rank: 5, matchedNumbers: [5, 17, 26] } }
-      ];
+        const cases = [
+          {
+            numbers: [2, 15, 17, 20, 33, 42],
+            round: 1047,
+            expectedResult: { rank: 4, matchedNumbers: [2, 20, 33, 42] }
+          },
+          {
+            numbers: [5, 17, 26, 19, 33, 41],
+            round: 1052,
+            expectedResult: { rank: 5, matchedNumbers: [5, 17, 26] }
+          },
+          {
+            numbers: [1, 5, 17, 26, 19, 33],
+            round: 1052,
+            expectedResult: { rank: 5, matchedNumbers: [5, 17, 26] }
+          },
+          {
+            numbers: [2, 22, 26, 29, 30, 34],
+            round: 1053,
+            expectedResult: { rank: 3, matchedNumbers: [22, 26, 29, 30, 34] }
+          },
+          {
+            numbers: [11, 23, 25, 30, 32, 42],
+            round: 1058,
+            expectedResult: { rank: 2, matchedNumbers: [11, 23, 25, 30, 32, 42] }
+          },
+          {
+            numbers: [9, 14, 24, 25, 33, 34],
+            round: 1059,
+            expectedResult: { rank: 0, matchedNumbers: [25, 34] }
+          },
+          {
+            numbers: [3, 8, 29, 31, 34, 45],
+            round: 1060,
+            expectedResult: { rank: 0, matchedNumbers: [3, 45] }
+          },
+          {
+            numbers: [9, 11, 27, 36, 38, 41],
+            round: 1061,
+            expectedResult: { rank: 0, matchedNumbers: [27] }
+          },
+          {
+            numbers: [3, 6, 22, 23, 24, 38],
+            round: 1063,
+            expectedResult: { rank: 1, matchedNumbers: [3, 6, 22, 23, 24, 38] }
+          }
+        ];
 
-      cases.map(async i => {
-        const result = await lottoService.check(i.myNumber, i.round);
-        expect(result).toEqual(i.expect);
-      });
-    },
-    seconds(30)
-  );
+        for (const { numbers, round, expectedResult } of cases) {
+          const result = await lottoService.check(numbers, round);
+          expect(result).toEqual(expectedResult);
+        }
+      },
+      seconds(60)
+    );
 
-  it(
-    'should throw an exception when pass invalid lotto numbers',
-    async () => {
+    it('should throw an exception when pass invalid lotto numbers', async () => {
       const lottoService = new LottoService(configs);
 
       try {
@@ -121,7 +152,6 @@ describe('lottoService', function () {
       } catch (e) {
         expect(e).toEqual(LottoError.InvalidLottoNumber());
       }
-    },
-    seconds(10)
-  );
+    });
+  });
 });
