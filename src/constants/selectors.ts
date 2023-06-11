73223449b9b952e09850813b59bca94e53f0b649
bang9 <gusrn1423@naver.com>
2023-06-11 15:06:08 +0900

    chore: skip

--- src/constants/selectors.ts
+++ src/constants/selectors.ts
@@ -3,10 +3,10 @@ export const SELECTORS = {
   PWD_INPUT:
     '#article > div:nth-child(2) > div > form > div > div.inner > fieldset > div.form > input[type=password]:nth-child(2)',
   LOGIN_BUTTON: '#article > div:nth-child(2) > div > form > div > div.inner > fieldset > div.form > a',
-  PURCHASE_TYPE_RANDOM_BTN: '#tabWay2Buy a#num2',
+  PURCHASE_TYPE_RANDOM_BTN: 'a[href="#divWay2Buy1"]#num2',
   PURCHASE_AMOUNT_SELECT: 'select#amoundApply',
-  PURCHASE_AMOUNT_CONFIRM_BTN: '#btnSelectNum',
-  PURCHASE_BTN: '#btnBuy',
-  PURCHASE_CONFIRM_BTN: '#popupLayerConfirm .btns > input[value="확인"]',
+  PURCHASE_AMOUNT_CONFIRM_BTN: 'input[value="확인"]#btnSelectNum',
+  PURCHASE_BTN: 'input[value="구매하기"]#btnBuy',
+  PURCHASE_CONFIRM_BTN: 'input[value="확인"][onclick="javascript:closepopupLayerConfirm(true);"]',
   PURCHASE_NUMBER_LIST: '#reportRow .nums'
 };
