136632f3edef40631e599d128ff257ea444b1ff0
bang9 <gusrn1423@naver.com>
2023-06-11 15:44:14 +0900

    revert user agent

--- src/controllers/playwright/index.ts
+++ src/controllers/playwright/index.ts
@@ -17,10 +17,7 @@ export class PlaywrightController implements BrowserControllerInterface {
     this.logger = logger;
     chromium.launch(this.configs).then(async browser => {
       this.browser = browser;
-      this.context = await browser.newContext({
-        userAgent:
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
-      });
+      this.context = await browser.newContext();
     });
   }
 
